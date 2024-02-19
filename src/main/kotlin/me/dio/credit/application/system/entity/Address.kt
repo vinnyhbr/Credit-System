c037a9cd2b59cb722b0e12f0443f5daa29e03378
vinnyhbr <113705995+vinnyhbr@users.noreply.github.com>
2024-02-19 17:09:11 -0300

    Sending all the modification

--- src/main/kotlin/me/dio/credit/application/system/entity/Address.kt
+++ src/main/kotlin/me/dio/credit/application/system/entity/Address.kt
@@ -1,3 +1,10 @@
 package me.dio.credit.application.system.entity
 
-data class Address()
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+
+@Embeddable
+data class Address(
+    @Column(nullable = false) var zipCode: String = "",
+    @Column(nullable = false) var street: String = ""
+)
